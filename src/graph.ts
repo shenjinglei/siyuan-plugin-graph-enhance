b12d7f347eb4bcbeb30286404e5b1f06fa36d68b
Jerry.Shen <shenjinglei2010@163.com>
2023-12-11 15:34:26 +0800

    refactoring: split sunbrust graph

--- src/graph.ts
+++ src/graph.ts
@@ -8,7 +8,6 @@ import {
     GraphChart,
     GraphSeriesOption,
     SunburstChart,
-    SunburstSeriesOption,
 } from "echarts/charts";
 import type {
     ComposeOption,
@@ -17,6 +16,7 @@ import type {
 import * as dagre from "@dagrejs/dagre";
 import { DagreNodeValue, DagreOutput } from "./types";
 import { TailGraph } from "./tail-graph";
+import { sunburstGraph } from "./sunburst-graph";
 
 const ColorJs = require("colorjs.io/dist/color.legacy.cjs").default;
 
@@ -26,18 +26,7 @@ echarts.use([
     CanvasRenderer
 ]);
 
-type ECOption = ComposeOption<
-    SunburstSeriesOption | GraphSeriesOption
->;
-
-interface SunburstNode {
-    id: string,
-    name: string,
-    children?: any[],
-    value?: number,
-    amount?: number,
-    height: number
-}
+type ECOption = ComposeOption<GraphSeriesOption>;
 
 interface EChartNode {
     id: string;
@@ -74,10 +63,6 @@ interface Palette {
 
 class EnhancedGraph {
     processedGraph: dagre.graphlib.Graph<DagreNodeValue>;
-    sourceNodes: string[];
-    sinkNodes: string[];
-    sourceGraphData: any = undefined;
-    sinkGraphData: any = undefined;
     sourceNodeId = "0";
     focusGraphType: "global" | "ancestor" | "brother" | "cross" | "neighbor" = "ancestor";
     diffuseGraphType: "source" | "sink" | "tail" = "source";
@@ -182,8 +167,8 @@ class EnhancedGraph {
                 .forEach(x => rawGraph.node(x.id).dailynote = true);
         }
 
-        this.sourceNodes = nodes.filter(x => /^ge-moc$/.test(x.label)).flatMap(x => rawGraph.inEdges(x.id) ?? []).map(x => x.v);
-        this.sinkNodes = nodes.filter(x => /^ge-tag$/.test(x.label)).flatMap(x => rawGraph.inEdges(x.id) ?? []).map(x => x.v);
+        sunburstGraph.sourceNodes = nodes.filter(x => /^ge-moc$/.test(x.label)).flatMap(x => rawGraph.inEdges(x.id) ?? []).map(x => x.v);
+        sunburstGraph.sinkNodes = nodes.filter(x => /^ge-tag$/.test(x.label)).flatMap(x => rawGraph.inEdges(x.id) ?? []).map(x => x.v);
 
         const nodesExclusionSetting = getSetting("nodesExclusion").split("\n");
         nodesExclusionSetting.push("^ge-moc|ge-tag$");
@@ -215,22 +200,10 @@ class EnhancedGraph {
             };
         }
 
-        this.sourceGraphData = undefined;
-        this.sinkGraphData = undefined;
+        sunburstGraph.sourceGraphData = undefined;
+        sunburstGraph.sinkGraphData = undefined;
     }
 
-    processSunburst() {
-        switch (this.diffuseGraphType) {
-            case "source":
-                if (!this.sourceGraphData)
-                    this.getSourceGraph();
-                break;
-            case "sink":
-                if (!this.sinkGraphData)
-                    this.getSinkGraph();
-                break;
-        }
-    }
 
     processGraph() {
         switch (this.focusGraphType) {
@@ -454,165 +427,6 @@ class EnhancedGraph {
     }
 
 
-    Threshold: number;
-
-    generteLeaf(cur: string, childrenNum: number) {
-        if (childrenNum >= this.Threshold) {
-            return {
-                id: cur,
-                name: rawGraph.node(cur).label,
-                value: 1,
-                height: 1,
-            };
-        } else {
-            return {
-                id: cur,
-                name: rawGraph.node(cur).label,
-                value: 0,
-                amount: childrenNum,
-                height: 0,
-            };
-        }
-    }
-
-    getNodeData(cur: string, level: number): SunburstNode {
-        if (level === 3) {
-            return this.generteLeaf(cur, this.getNodes(cur).length);
-        }
-
-        const children = this.getNodes(cur).map((x: string) => this.getNodeData(x, level + 1));
-
-        if (children.filter((x) => x.value === 0).length === children.length) {
-            return this.generteLeaf(cur, children.reduce((p, c) => p + (c.amount ?? 0), 0));
-        }
-
-        return {
-            id: cur,
-            name: rawGraph.node(cur).label,
-            children: children,
-            height: children.map(x => x.height).reduce((p, c) => p > c ? p : c, 0) + 1,
-        };
-    }
-
-    getNodes(cur: string): string[] {
-        if (this.diffuseGraphType === "source")
-            return (rawGraph.outEdges(cur) ?? []).map(x => x.w);
-        else
-            return (rawGraph.inEdges(cur) ?? []).map(x => x.v);
-    }
-
-    genSunburstData(param: SunburstNode[]) {
-        return [...param.filter(x => x.height === 3), {
-            name: "其他",
-            children: [...param.filter(x => x.height === 2), {
-                name: "其他",
-                children: param.filter(x => x.height === 1)
-            }]
-        }];
-    }
-
-    getSinkGraph() {
-        this.Threshold = Number(getSetting("sinkThreshold"));
-
-        if (this.sinkNodes.length === 0) {
-            //@ts-ignore
-            this.sinkNodes = rawGraph.sinks();
-        }
-
-        const result = this.sinkNodes
-            .filter(x => !/^\d{4}-\d{2}-\d{2}$/.test(rawGraph.node(x).label))
-            .map((x: any) => this.getNodeData(x, 1));
-
-        this.sinkGraphData = this.genSunburstData(result);
-    }
-
-    getSourceGraph() {
-        this.Threshold = Number(getSetting("sourceThreshold"));
-
-        if (this.sourceNodes.length === 0) {
-            //@ts-ignore
-            this.sourceNodes = rawGraph.sources();
-        }
-
-        const result = this.sourceNodes
-            .filter(x => !/^\d{4}-\d{2}-\d{2}$/.test(rawGraph.node(x).label))
-            .map((x: any) => this.getNodeData(x, 1));
-
-        this.sourceGraphData = this.genSunburstData(result);
-    }
-
-    sunbrushDisplay() {
-        this.processSunburst();
-
-        aEChart.clear();
-        aEChart.off("click");
-
-        const option: ECOption = {
-            series: {
-                type: "sunburst",
-
-                nodeClick: "link",
-                data: this.diffuseGraphType === "source" ? this.sourceGraphData : this.sinkGraphData,
-                radius: [0, "95%"],
-                sort: "desc",
-
-                emphasis: {
-                    focus: "descendant",
-                },
-
-                startAngle: 91,
-
-                levels: [
-                    {},
-                    {
-                        r0: "10%",
-                        r: "33%",
-                        itemStyle: {
-                            borderWidth: 2,
-                        },
-                        label: {
-                            align: "right",
-                            minAngle: 6,
-                        },
-                    },
-                    {
-                        r0: "33%",
-                        r: "70%",
-                        label: {
-                            align: "center",
-                            minAngle: 4,
-                        },
-                    },
-                    {
-                        r0: "70%",
-                        r: "72%",
-                        label: {
-                            position: "outside",
-                            padding: 3,
-                            silent: false,
-                            minAngle: 2,
-                        },
-                        itemStyle: {
-                            borderWidth: 3,
-                        },
-                    },
-                ],
-            },
-        };
-
-        option && aEChart.setOption(option);
-
-        aEChart.on("click", function (params: echarts.ECElementEvent) {
-            // @ts-ignore
-            const objId: string = params.data.id;
-            if (objId) {
-                openTab({ app: plugin.app, doc: { id: objId, action: ["cb-get-focus"] } });
-            }
-        });
-    }
-
-
-
     public Display() {
         if (this.sourceNodeId === "0") {
             showMessage(
@@ -710,7 +524,7 @@ class EnhancedGraph {
                 (new TailGraph()).draw();
                 break;
             default:
-                this.sunbrushDisplay();
+                sunburstGraph.draw();
         }
     }
 
